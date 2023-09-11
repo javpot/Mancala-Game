9b2aa0ec465bbee94ce7795c39757df993d534ea
Zafer A <lssjshisui@gmail.com>
2023-09-11 18:11:52 -0400

    turn based

--- MancalaGame.py
+++ MancalaGame.py
@@ -18,6 +18,8 @@ class Mancala:
         }
     )
 
+    turn = True
+
     def nouvelleGrille():
         nvGrille = dict(
             {
@@ -44,37 +46,50 @@ class Mancala:
         indexPuit = id
         lettrePuit = listPuits[id]
         nbGrainesPuit = Mancala.grille[lettrePuit]
-        if listPuits[id] in "ABCDEF":
-            for p in range(0, nbGrainesPuit + 1):
-                lettrePuit = listPuits[indexPuit]
-                nbGrainesPuit = Mancala.grille[lettrePuit]
-                # Gerer index > 13
-                if indexPuit >= 13:
-                    indexPuit = 0
-                    nbGrainesPuit += 1
-                elif indexPuit in range(0, 13):
-                    indexPuit += 1
-                    nbGrainesPuit += 1
-                    print(
-                        "DB label, nbGraines: ",
-                        lettrePuit,
-                        ", ",
-                        nbGrainesPuit,
-                    )
-                    print("DB positionCurrent: ", indexPuit)
-                Mancala.grille[lettrePuit] = nbGrainesPuit
-            Mancala.grille[listPuits[id]] = 0
-            print(Mancala.verifierGagnant())
+        if Mancala.turn is True:
+            if listPuits[id] in "ABCDEF":
+                for p in range(0, nbGrainesPuit + 1):
+                    lettrePuit = listPuits[indexPuit]
+                    nbGrainesPuit = Mancala.grille[lettrePuit]
+                    # Gerer index > 13
+                    if indexPuit >= 13:
+                        indexPuit = 0
+                        nbGrainesPuit += 1
+                    elif indexPuit in range(0, 13):
+                        indexPuit += 1
+                        nbGrainesPuit += 1
+                        print(
+                            "DB label, nbGraines: ",
+                            lettrePuit,
+                            ", ",
+                            nbGrainesPuit,
+                        )
+                        print("DB positionCurrent: ", indexPuit)
+                    Mancala.grille[lettrePuit] = nbGrainesPuit
+                Mancala.grille[listPuits[id]] = 0
+                Mancala.turn = False
+                print(Mancala.verifierGagnantJoueur())
 
     def ordiDeplacement():
+        Mancala.verifierGagnantOrdi()
         return False
 
-    def verifierGagnant():
+    def verifierGagnantJoueur():
         listPuits = list(Mancala.grille.values())
-        for x in range(0,6):
-            if(listPuits[x] == 0):
+        for x in range(0, 6):
+            if listPuits[x] == 0:
                 continue
             else:
                 return "Non gagnant"
-            
-        return "Player as won the game"
+
+        return "Player has won the game"
+
+    def verifierGagnantOrdi():
+        listPuits = list(Mancala.grille.values())
+        for x in range(7, 13):
+            if listPuits[x] == 0:
+                continue
+            else:
+                return "Non gagnant"
+
+        return "Ordi has won the game"
