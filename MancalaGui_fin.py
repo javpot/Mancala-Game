6a6b58caed57c59eb4f27517fae5570407ab6387
Zafer A <lssjshisui@gmail.com>
2023-10-23 15:41:18 -0400

    ecran fin de partiee fini

--- MancalaGui_fin.py
+++ MancalaGui_fin.py
@@ -30,9 +30,43 @@ titre = font.render(Mancala.verifierGagnant(), True, WHITE)
 titre_rect = titre.get_rect()
 titre_rect.center = (width // 2, 25)
 
-button = pygame.Rect(0, 0, 200, 50)  # Rectangle pour le bouton
-button.center = (button_x, button_y)
+# Label for "Voulez-vous jouer à nouveau?"
+rejouer_label = font.render("Voulez-vous jouer à nouveau?", True, WHITE)
+rejouer_label_rect = rejouer_label.get_rect()
+rejouer_label_rect.center = (width // 2, height - 150)
 
+# Liste des textes des boutons
+button_texts = ["Oui", "Non"]
+
+# Position de départ pour les boutons
+button_x = width // 2 - 112.5
+button_y = height - 100  # Centre les boutons verticalement
+
+# Création des boutons interactifs
+buttons = []
+for text in button_texts:
+    button = pygame.Rect(0, 0, 200, 50)  # Rectangle pour le bouton
+    button.center = (button_x, button_y)
+    buttons.append((button, text))
+    button_x += 225  # Espacement vertical
+oui_button_info = buttons[0]
+non_button_info = buttons[1]
+
+oui_button_rect, ordi_label = oui_button_info
+non_button_rect, joueur_label = non_button_info
+
+obj_oui_button_rect = pygame.Rect(
+    oui_button_rect.left,
+    oui_button_rect.top,
+    oui_button_rect.width,
+    oui_button_rect.height,
+)
+obj_non_button_rect = pygame.Rect(
+    non_button_rect.left,
+    non_button_rect.top,
+    non_button_rect.width,
+    non_button_rect.height,
+)
 
 # Boucle principale
 running = True
@@ -42,11 +76,22 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:  # Clic gauche de la souris
-                switch_to_options()  # Appel de la fonction pour changer de fenêtre
+                if non_button_rect.collidepoint(event.pos):
+                    exit()  # quitter
+                elif oui_button_rect.collidepoint(event.pos):
+                    switch_to_options()  # Appel de la fonction pour changer de fenêtre
 
     screen.fill(BLACK)  # Fond Noir
 
     screen.blit(titre, titre_rect)
+    screen.blit(rejouer_label, rejouer_label_rect)
+
+    # Affichage des boutons
+    for button, text in buttons:
+        pygame.draw.rect(screen, WHITE, button)
+        button_surface = font.render(text, True, BLACK)
+        button_rect = button_surface.get_rect(center=button.center)
+        screen.blit(button_surface, button_rect)
 
     pygame.display.flip()
 
